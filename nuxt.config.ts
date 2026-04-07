fc43a827febcee5fc208a3035b2e6b86e55e661a
Денис Кожевников <newlife_kod@mail.ru>
2026-04-07 15:45:07 +0300

    fix favicon path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,10 +11,10 @@ export default defineNuxtConfig({
     baseURL: '/wedding-invitation/',
     head: {
       link: [
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: './apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: './favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: './favicon-16x16.png' },
+        { rel: 'manifest', href: './site.webmanifest' },
       ],
       meta: [
         {property: 'og:site_name', content: 'Электронное приглашение'},
